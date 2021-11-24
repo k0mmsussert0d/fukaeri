c85a4579b2e05cc9714780dd030ca92396c4a60e
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-11-25 00:07:21 +0100

    Added demo run in main

--- cmd/fukaeri/main.go
+++ cmd/fukaeri/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/k0mmsussert0d/fukaeri/pkg/chanapi"
 )
 
 func main() {
-	chanapi.Threads("g")
+	chanapi.StartClient()
+	gThreads := chanapi.Threads("g")
+
+	for _, threadsOnPage := range gThreads {
+		for _, threadInfo := range threadsOnPage.Threads {
+			fmt.Print(chanapi.Thread("g", strconv.Itoa(threadInfo.No)))
+		}
+	}
 }
