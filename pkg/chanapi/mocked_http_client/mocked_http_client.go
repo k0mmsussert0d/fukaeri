6ef7769cc860581d54948298371d1095288e4fd1
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2022-03-06 21:26:42 +0000

    HTTP client for unit testing

---
+++ pkg/chanapi/mocked_http_client/mocked_http_client.go
@@ -0,0 +1,119 @@
+package mockedhttpclient
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/k0mmsussert0d/fukaeri/internal/log"
+)
+
+type Request struct {
+	method   string
+	endpoint string
+}
+
+type Response struct {
+	code    int
+	body    []byte
+	headers map[string][]string
+}
+
+type MockedHttpClient struct {
+	requests map[Request]Response
+}
+
+type MockedHttpClientBuilderRequestPart struct {
+	client  *MockedHttpClient
+	request *Request
+}
+
+func New() *MockedHttpClient {
+	return &MockedHttpClient{
+		requests: make(map[Request]Response),
+	}
+}
+
+func (client *MockedHttpClient) On(method, endpoint string) *MockedHttpClientBuilderRequestPart {
+	return &MockedHttpClientBuilderRequestPart{
+		client: client,
+		request: &Request{
+			method:   method,
+			endpoint: endpoint,
+		},
+	}
+}
+
+func (builder *MockedHttpClientBuilderRequestPart) Return(code int, body []byte, headers map[string][]string) *MockedHttpClient {
+	builder.client.requests[*builder.request] = Response{
+		code:    code,
+		body:    body,
+		headers: headers,
+	}
+	return builder.client
+}
+
+var httpStatuses = map[int]string{
+	200: "200 OK",
+	201: "201 Created",
+	202: "202 Accepted",
+	204: "204 No Content",
+	301: "301 Moved Permanently",
+	302: "302 Found",
+	304: "304 Not Modified",
+	307: "307 Temporary Redirect",
+	308: "308 Permanent Redirect",
+	400: "400 Bad Request",
+	401: "401 Unauthrized",
+	403: "403 Forbidden",
+	404: "404 Not Found",
+	405: "405 Method Not Allowed",
+	408: "408 Request Timeout",
+	411: "411 Length Required",
+	413: "413 Payload Too Large",
+	414: "414 Request-URI Too Long",
+	415: "415 Unsupported Media Type",
+	429: "429 Too Many Requests",
+	500: "500 Internal Server Error",
+	502: "502 Bad Gateway",
+	503: "503 Service Unavailable",
+	504: "504 Gateway Timeout",
+}
+
+func (client *MockedHttpClient) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	method, host := req.Method, req.Host
+	response, exists := client.requests[Request{method, host}]
+	if !exists {
+		log.Error().Panicf("Response to %v %v has not been defined", method, host)
+	}
+
+	responseStatusString, exists := httpStatuses[response.code]
+	if !exists {
+		responseStatusString = fmt.Sprintf("%v", response.code)
+	}
+
+	var httpResponse *http.Response
+	httpResponse = &http.Response{
+		Status:     responseStatusString,
+		StatusCode: response.code,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     response.headers,
+		Body: struct {
+			io.Reader
+			io.Closer
+		}{
+			bytes.NewReader(response.body),
+			httpResponse.Body,
+		},
+		ContentLength: int64(len(response.body)),
+		Close:         false,
+		Uncompressed:  true,
+		Request:       req,
+	}
+
+	return httpResponse, nil
+}
